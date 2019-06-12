ef74fb285e63ae355aa22ebdca4bd640ef9f7695
George Lioris <george.lioris@gmail.com>
2019-06-12 16:32:56 +0300

    removed useless scripts

--- scripts/cal_widget.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# This is widget to show a calendar
-alacritty -title="calwidget" --class="calwidget" -e sh -c ' printf "\\e]11;#0d0b0a\\e\\\\" && tput civis && PS1="" && stty -echo && clear && cal' &
-sleep 3
-i3-msg [title="calwidget"] focus, floating enable, resize set 188 175, move position 0 0
-
-# Right = 1240
