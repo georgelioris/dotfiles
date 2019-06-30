38a1ef7ca00bdbe6d45e5b39950bb6ead5c30be7
George Lioris <george.lioris@gmail.com>
2019-07-01 00:41:32 +0300

    dopterm

---
+++ dropterm.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+## Check if a dropterm already exists
+## If it does do nothing
+## Else create a new instance
+
+DROP=$(pgrep -axfc "alacritty --class dropdown --config-file /home/void/.config/alacritty/drop.yml")
+
+if [ $DROP -gt 0 ]
+ then
+   exit 1
+  else
+    exec alacritty --class dropdown --config-file /home/void/.config/alacritty/drop.yml
+fi
